4567526a1c6db2a4d8592e43aa81f80d7f8b6ecf
Ben Wanninger(h) <bewanninger@gmail.com>
2013-11-19 09:59:19 -0500

    Minor Tweak

--- module/Lyrics/config/module.config.php
+++ module/Lyrics/config/module.config.php
@@ -11,11 +11,9 @@ return array(
              'lyrics' => array(
                  'type'    => 'segment',
                  'options' => array(
-                     'route'    => '/lyrics[/][:action][/:id][/:quantity]',
+                     'route'    => '/lyrics[/][:action]',
                      'constraints' => array(
                          'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                         'id'     => '[0-9]+',
-                         'quantity'     => '[0-9]',
                      ),
                      'defaults' => array(
                          'controller' => 'Lyrics\Controller\Lyrics',
@@ -30,7 +28,5 @@ return array(
         'template_path_stack' => array(
             'lyrics' => __DIR__ . '/../view',
         ),
-    ),
-
-     
+    ), 
 );
